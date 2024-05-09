1581884055db58cfc85d81f1c699ca9bd4667944
Aris <97361244+Mentem-Auream@users.noreply.github.com>
2024-05-09 23:22:57 +0200

    Site

---
+++ JavaScript/navbar.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', function() {
+    var logo = document.querySelector('.logo');
+    var navLinks = document.querySelectorAll('.nav__links a');
+    
+    if (window.scrollY > 650) {
+        logo.style.color = '#0a0a0a'; // Zmiana koloru tekstu klasy .logo na czarny
+        navLinks.forEach(function(link) {
+            link.style.color = '#0a0a0a'; // Zmiana koloru tekstu nawigacji na czarny
+        });
+    } else {
+        logo.style.color = '#fff'; // Powrót do pierwotnego koloru tekstu klasy .logo
+        navLinks.forEach(function(link) {
+            link.style.color = '#fff'; // Powrót do pierwotnego koloru tekstu nawigacji
+        });
+    }
+});
