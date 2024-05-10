c2f4673df443c49f450bd23f6184bc3a0e574938
Aris <97361244+Mentem-Auream@users.noreply.github.com>
2024-05-10 12:00:27 +0200

    23

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function() {
     var logo = document.querySelector('.logo');
     var navLinks = document.querySelectorAll('.nav__links a');
     
-    if (window.scrollY > 650) {
+    if (window.scrollY > 600) {
         logo.style.color = '#0a0a0a'; // Zmiana koloru tekstu klasy .logo na czarny
         navLinks.forEach(function(link) {
             link.style.color = '#0a0a0a'; // Zmiana koloru tekstu nawigacji na czarny
